01cb5a12f523232e4f3d44d77554876147b85fda
David Barnes <david.o.barnes@timkensteel.com>
2021-11-22 16:48:21 -0500

    Add project files.

---
+++ WorkRequest/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WorkRequest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Update = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            this.Submit = new System.Windows.Forms.Button();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbSubArea = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbArea = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbEmailTo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 151);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(999, 673);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Update);
+            this.groupBox1.Controls.Add(this.Refresh);
+            this.groupBox1.Controls.Add(this.Submit);
+            this.groupBox1.Controls.Add(this.tbDescription);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.cbType);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cbSubArea);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbArea);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbStatus);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 113);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entry Form";
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(172, 84);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 12;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(91, 84);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 11;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(10, 84);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 10;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(79, 47);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(690, 20);
+            this.tbDescription.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Description: ";
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Location = new System.Drawing.Point(649, 17);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(120, 21);
+            this.cbType.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(606, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Type: ";
+            // 
+            // cbSubArea
+            // 
+            this.cbSubArea.FormattingEnabled = true;
+            this.cbSubArea.Location = new System.Drawing.Point(447, 17);
+            this.cbSubArea.Name = "cbSubArea";
+            this.cbSubArea.Size = new System.Drawing.Size(153, 21);
+            this.cbSubArea.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sub Area: ";
+            // 
+            // cbArea
+            // 
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Location = new System.Drawing.Point(233, 17);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(145, 21);
+            this.cbArea.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Area: ";
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Location = new System.Drawing.Point(79, 17);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(98, 21);
+            this.cbStatus.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.cbEmailTo);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(811, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Email";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EmailSend_Click);
+            // 
+            // cbEmailTo
+            // 
+            this.cbEmailTo.FormattingEnabled = true;
+            this.cbEmailTo.Location = new System.Drawing.Point(10, 37);
+            this.cbEmailTo.Name = "cbEmailTo";
+            this.cbEmailTo.Size = new System.Drawing.Size(184, 21);
+            this.cbEmailTo.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Recipient:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 834);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Work Requests";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbSubArea;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbArea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Refresh;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbEmailTo;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WorkRequest/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WorkRequest
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=HSPHMLTSQL2;Initial Catalog=HSPMelt;Persist Security Info=False;User ID=HSPMelt_rw_datauser;Password=HSPMelt_rw_datauser";
+        string querySelect = null;
+        string queryDropDownSelect = null;
+        string queryInsert = null;
+        string queryUpdate = null;
+        DataTable data = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetData();
+        }
+        private void GetData()
+        {
+            querySelect = $"SELECT * FROM WorkRequests order by DateTime desc";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(querySelect, conn);
+
+                data = new DataTable();
+                adapter.Fill(data);
+
+                dataGridView1.DataSource = data;
+            }
+
+            queryDropDownSelect = "SELECT DISTINCT Status FROM WorkRequests ORDER BY Status asc ";
+            cbStatus.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryDropDownSelect, conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+
+                while (DR.Read())
+                { cbStatus.Items.Add(DR[0]); }
+            }
+
+            queryDropDownSelect = "SELECT DISTINCT [Area] FROM WorkRequests ORDER BY Area asc ";
+            cbArea.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryDropDownSelect, conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+
+                while (DR.Read())
+                { cbArea.Items.Add(DR[0]); }
+            }
+
+            queryDropDownSelect = "SELECT DISTINCT [SubArea] FROM WorkRequests ORDER BY SubArea asc ";
+            cbSubArea.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryDropDownSelect, conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+
+                while (DR.Read())
+                { cbSubArea.Items.Add(DR[0]); }
+            }
+
+            queryDropDownSelect = "SELECT DISTINCT [Type] FROM WorkRequests ORDER BY Type asc ";
+            cbType.Items.Clear();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryDropDownSelect, conn);
+                SqlDataReader DR = cmd.ExecuteReader();
+
+                while (DR.Read())
+                { cbType.Items.Add(DR[0]); }
+            }
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            queryInsert = $"INSERT INTO WorkRequests (DateTime, Status, Area, SubArea, Type, Description)  " +
+                   $"VALUES ('{DateTime.Now}', '{cbStatus.Text}', '{cbArea.Text}', '{cbSubArea.Text}', '{cbType.Text}', '{tbDescription.Text}');";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryInsert, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+                GetData();
+            }
+
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            queryUpdate = $"UPDATE WorkRequests " +
+                $"SET Status = '{cbStatus.Text}', Area = '{cbArea.Text}', SubArea = '{cbSubArea.Text}', Type = '{cbType.Text}', Description = '{tbDescription.Text}'" +
+                $"WHERE ID = {dataGridView1.SelectedRows[0].Cells[0].Value}";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(queryUpdate, conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+
+                GetData();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                cbStatus.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                cbArea.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                cbSubArea.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+                cbType.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+                tbDescription.Text = dataGridView1.SelectedRows[0].Cells[6].Value.ToString();
+            }
+        }
+
+        public void Email(string message, string ToRecipient)
+        {
+            System.Net.Mail.MailMessage Email = new System.Net.Mail.MailMessage();
+            Email.From = new System.Net.Mail.MailAddress("barnesaa", "List update");
+            Email.To.Add(ToRecipient);
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("Steelrelay.steel.net");
+            Email.BodyEncoding = Encoding.ASCII;
+            Email.Body = "test text.";//message;
+            client.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
+            client.Send(Email);
+        }
+
+        private void EmailSend_Click(object sender, EventArgs e)
+        {
+            Email($"{data}", $"{cbEmailTo.Text}");
+        }
+    }
+}
